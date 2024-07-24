85f6ccfe08f9228a39b31a5861cb6581d6d03567
Susana Eguis <158485971+suueguis@users.noreply.github.com>
2024-07-23 22:38:46 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+/* Adjunto link del documento. Al haber trabajado en conjunto, hemos utilizado
+diferentes equipos, por lo que no hemos concatenado los ejemplos. De igual manera,
+se subirá este archivo como evidencia del punto 16 de subir el repositorio.
+
+https://docs.google.com/document/d/1h7kpdUDG-LcsEo4Ovquq6tNfsAt2vJdJ5bA_Z-c0D7g/edit?usp=sharing
+
+Gracias :)
+ */
